fc17e0b5c6fb3dcf09416aa418d777360017cf4f
shahriarahmadf <shahriarahmadf@gmail.com>
2023-03-06 20:02:24 +0900

    res sent for courseDetails page by filtering course data

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get('/courses', (req,res) => {
     res.send(courses);
 })
 
+app.get('/courses/:courses_id', (req,res) => {
+    const courses_id = req.params.course_id;
+    const selectedCourse = courses.find(course => course.courses_id === courses_id);
+    res.send(selectedCourse);
+})
+
 app.listen(port, () => {
     console.log('engineering ed server running on port: ',port);
 })
